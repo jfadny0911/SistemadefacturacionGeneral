10e7a275f66397f6cdb63c87983fd9eae62f4ec2
jfadny0911 <116517266+jfadny0911@users.noreply.github.com>
2026-07-04 20:05:07 -0600

    Update login.py

--- auth/login.py
+++ auth/login.py
@@ -1,1 +1,56 @@
+import streamlit as st
 
+from database.connection import get_connection
+from auth.security import verify_password
+
+
+def login():
+
+    st.title("PG Manager")
+
+    email = st.text_input("Correo")
+
+    password = st.text_input(
+        "Contraseña",
+        type="password"
+    )
+
+    if st.button("Iniciar sesión"):
+
+        conn = get_connection()
+
+        cursor = conn.cursor()
+
+        cursor.execute(
+            """
+            SELECT
+                id,
+                full_name,
+                password_hash
+
+            FROM users
+
+            WHERE email=%s
+            """,
+            (email,)
+        )
+
+        user = cursor.fetchone()
+
+        cursor.close()
+
+        conn.close()
+
+        if user:
+
+            if verify_password(password, user[2]):
+
+                st.session_state.logged = True
+
+                st.session_state.user_id = user[0]
+
+                st.session_state.user_name = user[1]
+
+                st.rerun()
+
+        st.error("Correo o contraseña incorrectos")
