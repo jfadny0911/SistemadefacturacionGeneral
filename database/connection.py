2e63e5807a0e79f320637180aac19c3fbbbcc219
jfadny0911 <116517266+jfadny0911@users.noreply.github.com>
2026-07-04 20:00:46 -0600

    Convert DB port to integer in connection function

--- database/connection.py
+++ database/connection.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import psycopg2
 
+
 def get_connection():
     return psycopg2.connect(
         host=st.secrets["DB_HOST"],
         database=st.secrets["DB_NAME"],
         user=st.secrets["DB_USER"],
         password=st.secrets["DB_PASSWORD"],
-        port=st.secrets["DB_PORT"],
+        port=int(st.secrets["DB_PORT"]),
         sslmode=st.secrets["DB_SSLMODE"],
     )
